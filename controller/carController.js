5c22257b01c96814e5db0d96ae7e601a0d031678
Idle del <magarkiran436@gmail.com>
2025-07-17 12:41:19 +0545

    updated car from form in postman

--- controller/carController.js
+++ controller/carController.js
@@ -1,12 +1,15 @@
 import Car from "../models/carModel.js";
 
+// Get all cars
 const getAllCars = async (req, res) => {
   const cars = await Car.find();
   res.status(200).json(cars);
 };
 
+// Add a new car
 const addCar = async (req, res) => {
   const { name, model, brand, rentPerDay, location, status } = req.body;
+
   if (
     !name ||
     !model ||
@@ -14,16 +17,12 @@ const addCar = async (req, res) => {
     !rentPerDay ||
     !status ||
     !location ||
-    !req.file 
+    !req.file
   ) {
-    return res.status(400).json({
-      message: "All fields are Mandatory ! ",
-    });
+    return res.status(400).json({ message: "All fields are mandatory!" });
   }
-  let image = "";
-    if(req.file){
-        image = req.file.filename;
-    }
+
+  const image = req.file.filename;
 
   const car = await Car.create({
     name,
@@ -33,55 +32,72 @@ const addCar = async (req, res) => {
     status,
     location,
     user_id: req.user.id,
-    image: image
+    image,
   });
+
   res.status(201).json(car);
 };
 
+// Get single car
 const getCar = async (req, res) => {
   const car = await Car.findById(req.params.id);
   if (!car) {
-    return res.status(404).json({
-      message: "Car not found ! ",
-    });
+    return res.status(404).json({ message: "Car not found!" });
   }
   res.status(200).json(car);
 };
 
+// Update car
 const updateCar = async (req, res) => {
   const car = await Car.findById(req.params.id);
   if (!car) {
-    return res.status(404).json({
-      messsage: "Car not found",
-    });
+    return res.status(404).json({ message: "Car not found" });
   }
 
   if (car.user_id.toString() !== req.user.id) {
-    return res.status(403).json({
-      messsage: "Don't have permission to update the car !",
-    });
+    return res
+      .status(403)
+      .json({ message: "Don't have permission to update the car!" });
   }
 
-  const updatedCar = await Car.findByIdAndUpdate(req.params.id, req.body, {
+  const { name, model, brand, rentPerDay, location, status } = req.body;
+
+  let image = car.image;
+  if (req.file) {
+    image = req.file.filename;
+  }
+
+  const updatedFields = {
+    name: name || car.name,
+    model: model || car.model,
+    brand: brand || car.brand,
+    rentPerDay: rentPerDay || car.rentPerDay,
+    location: location || car.location,
+    status: status || car.status,
+    image,
+  };
+
+  const updatedCar = await Car.findByIdAndUpdate(req.params.id, updatedFields, {
     new: true,
   });
   res.status(200).json(updatedCar);
 };
 
+// Delete car
 const deleteCar = async (req, res) => {
   const car = await Car.findById(req.params.id);
   if (!car) {
-    return res.status(404).json({
-      message: "Car not found !",
-    });
+    return res.status(404).json({ message: "Car not found!" });
   }
+
   if (car.user_id.toString() !== req.user.id) {
-    return res.status(403).json({
-      message: "Don't have permission to delete the car !",
-    });
+    return res
+      .status(403)
+      .json({ message: "Don't have permission to delete the car!" });
   }
+
   await car.deleteOne();
-  res.status(200).json(car);
+  res.status(200).json({ message: "Car deleted successfully", car });
 };
 
 export { getAllCars, addCar, getCar, updateCar, deleteCar };
--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,18 +1,27 @@
 import express from "express";
-import { addCar, getAllCars, getCar, updateCar, deleteCar } from "../controller/carController.js";
+import {
+  addCar,
+  getAllCars,
+  getCar,
+  updateCar,
+  deleteCar,
+} from "../controller/carController.js";
 import { isAdmin, isLoggedIn } from "../middleware/userMiddleware.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
-router.route("/").get(isLoggedIn,getAllCars);
-router.route("/addCar").post(isLoggedIn, isAdmin, upload.single("image"), addCar);
-// router.route("/addCar").post(isLoggedIn, isAdmin, addCar);
+router.route("/").get(isLoggedIn, getAllCars);
+
+router
+  .route("/addCar")
+  .post(isLoggedIn, isAdmin, upload.single("image"), addCar);
 router.route("/:id").get(isLoggedIn, getCar);
-router.route("/:id").put(isLoggedIn, isAdmin, updateCar);
+
+router
+  .route("/:id")
+  .put(isLoggedIn, isAdmin, upload.single("image"), updateCar);
+
 router.route("/:id").delete(isLoggedIn, isAdmin, deleteCar);
-
-
-
 
 export default router;
